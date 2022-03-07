556aeb3d43d61d388ce931fa8b39f6927edae0f5
darshan <darshanradadiya98@gmail.com>
2022-03-06 21:13:53 -0800

    added result page and show country list in dropdown

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# read Json file
+with open('./Address_details.json','r') as file:
+    data = json.load(file)
+
 @app.route('/')
 def home():
-    return render_template('app.html')
+    # Get the list of countries from the json file.
+    countries = [a_dict["Country"] for a_dict in data]
+
+    return render_template('app.html', title="Home Page", countries=set(countries))
+
+@app.route('/result_page',  methods=["POST"] )
+def show_address():
+
+    Country = request.form.get("country")
+    State = request.form.get("State")
+    Address_line_1 = request.form.get("Address_line_1")
+    Address_Line_2 = request.form.get("Address_Line_2")
+    Zipcode = request.form.get("Zipcode")
+    ph_no = request.form.get("ph_no")
+
+    address = Country  + State +  Address_line_1 + Address_Line_2 + Zipcode + ph_no 
+
+    return render_template('result.html', title="Result Page", Address = address)
+
