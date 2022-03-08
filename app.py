089a737e8874803a4da9dcbf617ef941eb48ee34
darshan <darshanradadiya98@gmail.com>
2022-03-08 14:47:48 -0800

    added matching address functionality ..

--- app.py
+++ app.py
@@ -8,18 +8,16 @@ app = Flask(__name__)
 with open('./Address_details.json', 'r') as file:
     data = json.load(file)
 
-
 @app.route('/')
 def home():
     # Get the list of countries from the json file.
     countries = [a_dict["Country"] for a_dict in data]
-
     return render_template('app.html', title="Home Page", countries=set(countries))
-
 
 @app.route('/result_page',  methods=["POST"])
 def show_address():
 
+    # get the address detail from HTML form .
     Address_line_1 = request.form.get("Address_line_1")
     Address_Line_2 = request.form.get("Address_Line_2")
     State = request.form.get("State")
@@ -30,16 +28,18 @@ def show_address():
     
     if County == None:
         County = ""
+    
+    match_found = False
+    matching_add = ""
 
     user_entered_address = {"Address_line_1":Address_line_1.title(), "Address_Line_2":Address_Line_2.title(), "State":State.title(), "County":County.title(), "Zipcode":Zipcode.title(), "Country":Country, "ph_no":ph_no}    
 
-    match_found = False
-
     if Country == "USA":
         user_entered_address.pop("County")
-
     for i  in data:
         if i == user_entered_address:
             match_found = TRUE
+        if i["Address_line_1"] == Address_line_1.title():
+            matching_add = ", ".join(i.values())
 
-    return render_template('result.html', title="Result Page", Address_line_1_value=Address_line_1, Address_Line_2_value=Address_Line_2, State_value=State, County_value=County, Zipcode_value=Zipcode, Country_value=Country, ph_no_value=ph_no, address_match = match_found)
+    return render_template('result.html', title="Result Page", Address_line_1_value=Address_line_1, Address_Line_2_value=Address_Line_2, State_value=State, County_value=County, Zipcode_value=Zipcode, Country_value=Country, ph_no_value=ph_no, address_match = match_found, matching = matching_add)
